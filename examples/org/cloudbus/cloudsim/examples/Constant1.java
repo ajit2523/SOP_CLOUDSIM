2b8aee9c713ebeed24ac6efd628ee698169fa2cf
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-03-31 17:30:56 +0530

    Paper 1

--- examples/org/cloudbus/cloudsim/examples/Constant1.java
+++ examples/org/cloudbus/cloudsim/examples/Constant1.java
@@ -5,7 +5,7 @@ public class Constant1 {
     public static final int NORMAL_MACHINES = 80;
 
     public static final int MACHINES = SPECIAL_MACHINES + NORMAL_MACHINES;
-    public static int PRIORITY_SPECIAL=1;
-    public static int PRIORITY_NORMAL=0;
-    public static long LENGTH_FACTOR=10000;
+    public static int PRIORITY_SPECIAL = 1;
+    public static int PRIORITY_NORMAL = 0;
+    public static long LENGTH_FACTOR = 10000;
 }
