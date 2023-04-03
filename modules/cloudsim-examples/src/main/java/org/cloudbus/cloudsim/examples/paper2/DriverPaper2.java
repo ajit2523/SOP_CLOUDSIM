04f4ea239294871eab1629ac7c9dc643b2f2fba4
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-04-03 22:49:24 +0530

    Paper 2 Init

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/paper2/DriverPaper2.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/paper2/DriverPaper2.java
@@ -13,7 +13,6 @@ package org.cloudbus.cloudsim.examples.paper2;
 import java.text.DecimalFormat;
 import java.util.*;
 
-import org.cloudbus.cloudsim.Cloudlet;
 import org.cloudbus.cloudsim.CloudletSchedulerTimeShared;
 import org.cloudbus.cloudsim.Datacenter;
 import org.cloudbus.cloudsim.DatacenterBroker;
@@ -39,7 +38,7 @@ import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
 public class DriverPaper2 {
 
     /** The cloudlet list. */
-    private static List<Cloudlet> cloudletList;
+    private static List<CustomCloudlet> cloudletList;
 
     /** The vmlist. */
     private static List<Vm> vmlist;
@@ -53,7 +52,7 @@ public class DriverPaper2 {
         long size = 10000; //image size (MB)
         int ram = 512; //vm memory (MB)
         int mips = 1000;
-        long bw = 1000;
+        long bw = 1000; // should be 5
         int pesNumber = 1; //number of cpus
         String vmm = "Xen"; //VMM name
 
@@ -72,9 +71,9 @@ public class DriverPaper2 {
     }
 
 
-    private static List<Cloudlet> createCloudlet(int userId, int cloudlets){
+    private static List<CustomCloudlet> createCloudlet(int userId, int cloudlets){
         // Creates a container to store Cloudlets
-        LinkedList<Cloudlet> list = new LinkedList<Cloudlet>();
+        LinkedList<CustomCloudlet> list = new LinkedList<CustomCloudlet>();
 
         //cloudlet parameters
         long length = 20;
@@ -83,13 +82,16 @@ public class DriverPaper2 {
         int pesNumber = 1;
         UtilizationModel utilizationModel = new UtilizationModelFull();
 
-        Cloudlet[] cloudlet = new Cloudlet[cloudlets];
+        CustomCloudlet[] cloudlet = new CustomCloudlet[cloudlets];
 
         for(int i=0;i<cloudlets;i++){
             Random rand = new Random();
-            cloudlet[i] = new Cloudlet(i, length + rand.nextInt(180), pesNumber, fileSize, outputSize, utilizationModel, utilizationModel, utilizationModel);
+            cloudlet[i] = new CustomCloudlet(i, length + rand.nextInt(180), pesNumber, fileSize, outputSize, utilizationModel, utilizationModel, utilizationModel);
             // setting the owner of these Cloudlets
             cloudlet[i].setUserId(userId);
+            //set deadline for each cloudlet randomly between 200 and 2000 ms
+            cloudlet[i].setDeadline(200 + rand.nextDouble(1800));
+            cloudlet[i].setRandomPriority();
             list.add(cloudlet[i]);
         }
 
@@ -120,7 +122,7 @@ public class DriverPaper2 {
             @SuppressWarnings("unused")
             Datacenter datacenter0 = createDatacenter("Datacenter_0");
             @SuppressWarnings("unused")
-            Datacenter datacenter1 = createDatacenter("Datacenter_1");
+//            Datacenter datacenter1 = createDatacenter("Datacenter_1");
 
             //Third step: Create Broker
             DatacenterBroker broker = createBroker();
@@ -137,7 +139,7 @@ public class DriverPaper2 {
             CloudSim.startSimulation();
 
             // Final step: Print results when simulation is over
-            List<Cloudlet> newList = broker.getCloudletReceivedList();
+            List<CustomCloudlet> newList = broker.getCloudletReceivedList();
 
             CloudSim.stopSimulation();
 
@@ -164,26 +166,26 @@ public class DriverPaper2 {
         //    a Machine.
         List<Pe> peList1 = new ArrayList<Pe>();
 
-        int mips = 1000;
+        int mips = 10000;
 
         // 3. Create PEs and add these into the list.
         //for a quad-core machine, a list of 4 PEs is required:
         peList1.add(new Pe(0, new PeProvisionerSimple(mips))); // need to store Pe id and MIPS Rating
-        peList1.add(new Pe(1, new PeProvisionerSimple(mips)));
-        peList1.add(new Pe(2, new PeProvisionerSimple(mips)));
-        peList1.add(new Pe(3, new PeProvisionerSimple(mips)));
-
-        //Another list, for a dual-core machine
-        List<Pe> peList2 = new ArrayList<Pe>();
-
-        peList2.add(new Pe(0, new PeProvisionerSimple(mips)));
-        peList2.add(new Pe(1, new PeProvisionerSimple(mips)));
+//        peList1.add(new Pe(1, new PeProvisionerSimple(mips)));
+//        peList1.add(new Pe(2, new PeProvisionerSimple(mips)));
+//        peList1.add(new Pe(3, new PeProvisionerSimple(mips)));
+//
+//        //Another list, for a dual-core machine
+//        List<Pe> peList2 = new ArrayList<Pe>();
+//
+//        peList2.add(new Pe(0, new PeProvisionerSimple(mips)));
+//        peList2.add(new Pe(1, new PeProvisionerSimple(mips)));
 
         //4. Create Hosts with its id and list of PEs and add them to the list of machines
         int hostId=0;
         int ram = 2048; //host memory (MB)
         long storage = 1000000; //host storage
-        int bw = 10000;
+        int bw = 10000; // should be 5 megahertz
 
         hostList.add(
                 new Host(
@@ -270,9 +272,9 @@ public class DriverPaper2 {
      * Prints the Cloudlet objects
      * @param list  list of Cloudlets
      */
-    private static void printCloudletList(List<Cloudlet> list) {
+    private static void printCloudletList(List<CustomCloudlet> list) {
         int size = list.size();
-        Cloudlet cloudlet;
+        CustomCloudlet cloudlet;
 
         String indent = "    ";
         Log.printLine();
@@ -285,7 +287,7 @@ public class DriverPaper2 {
             cloudlet = list.get(i);
             Log.print(indent + cloudlet.getCloudletId() + indent + indent);
 
-            if (cloudlet.getCloudletStatus() == Cloudlet.SUCCESS){
+            if (cloudlet.getCloudletStatus() == CustomCloudlet.SUCCESS){
                 Log.print("SUCCESS");
 
                 Log.printLine( indent + indent + cloudlet.getResourceId() + indent + indent + indent + cloudlet.getVmId() +
